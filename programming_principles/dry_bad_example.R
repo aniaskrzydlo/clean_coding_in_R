933120eab0b2496b9a99dc3d65f6e15d6c85e9da
aniaskrzydlo <anna.skrzydlo@appsilon.com>
2023-02-11 18:59:40 +0100

    Added programming principles examples

---
+++ programming_principles/dry_bad_example.R
@@ -0,0 +1,8 @@
+radius_1 <- 4
+area_1 <- pi * radius_1^2
+
+radius_2 <- 6
+aread_2 <- pi * radius_2^2
+
+radius_3 <- 8
+aread_3 <- pi * radius_3^2
---
+++ programming_principles/kiss_bad_example.R
@@ -0,0 +1,19 @@
+get_day_of_weeek_name <- function(day_of_week_number) {
+  if (day_of_week_number == 1) {
+    "Monday"
+  } else if (day_of_week_number == 2) {
+    "Tuesday"
+  } else if (day_of_week_number == 3) {
+    "Wednesday"
+  } else if (day_of_week_number == 4) {
+    "Thursday"
+  } else if (day_of_week_number == 5) {
+    "Friday"
+  } else if (day_of_week_number == 6) {
+    "Saturday"
+  } else if (day_of_week_number == 7) {
+    "Sunday"
+  } 
+}
+
+get_day_of_weeek_name(5)
