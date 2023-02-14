a2ec2e196acdb36d6e89078bca4740fc09f91e0c
aniaskrzydlo <anna.skrzydlo@appsilon.com>
2023-02-14 16:50:31 +0100

    Task 3 resolved

--- exercises/task_3 Code Style to Improve.R
+++ exercises/task_3 Code Style to Improve.R
@@ -1,18 +1,17 @@
-rawData<-iris
-
-
-
-Selection = function(d , cols){
-  return(d[,cols])
-}
-data <- Selection (
-rawData,
-c("Species","Sepal.Length","Sepal.Width")
-)
-colnames( data )=c('species','length','width')
-
 library(dplyr)
 library(ggplot2)
-data%>%
-  filter(length>5)%>%
-  ggplot( aes(x=width , y=length , color=species)) + geom_point() + ggtitle('Sepal length and width coloured by species')
+
+raw_data <- iris
+
+select_columns <- function(d, cols) {
+  d[,cols]
+}
+
+data <- select_columns(raw_data, c("Species", "Sepal.Length", "Sepal.Width"))
+colnames(data) <- c("species", "length", "width")
+
+data %>%
+  filter(length > 5) %>%
+  ggplot(aes(x = width, y = length, color = species)) +
+  geom_point() +
+  ggtitle("Sepal length and width coloured by species")
